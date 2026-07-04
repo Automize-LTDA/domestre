5abed9e7cfd02e8d3daa68a61fe06bd377a750f2
Automize <automize@automize.com>
2026-07-03 23:14:49 -0300

    feat: add global test version welcome modal with developer credits

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AuthProvider } from './context/AuthContext'
 import { ToastProvider } from './context/ToastContext'
+import { TestVersionWelcome } from './components/TestVersionWelcome'
 
 // Import Pages
 import Dashboard from './pages/Dashboard'
@@ -30,6 +31,7 @@ const App: React.FC = () => {
       <ToastProvider>
         <AuthProvider>
           <BrowserRouter>
+            <TestVersionWelcome />
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/login" element={<Login />} />
